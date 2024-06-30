f560c9fa966b17ba0ad87e02333111bbbaaabe44
zoubjd <zouhairbajdouri19@gmail.com>
2024-06-30 05:04:10 +0100

    last push

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,7 @@
 #!/usr/bin/pup
-# install flask from pip
+# intalled flask from pip3.
 
-exec { 'flask':
-    ensure => '2.1.0',
-    provider => 'pip3',
+package {'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
 }
