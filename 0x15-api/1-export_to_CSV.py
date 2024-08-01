197d35c3fcf8dd4002b6e0e3d4caf96747213dbb
zoubjd <zouhairbajdouri19@gmail.com>
2024-08-01 01:32:23 +0100

    push

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""models doc"""
+import requests as req
+import sys
+
+
+if __name__ == "__main__":
+    id_employee = int(sys.argv[1])
+
+    url = "https://jsonplaceholder.typicode.com"
+    url_user = req.get(f"{url}/users/{id_employee}").json()
+    url_user_todos = req.get(f"{url}/users/{id_employee}/todos").json()
+
+    username = url_user.get("username")
+    filename = f"{id_employee}.csv"
+    with open(filename, 'w', encoding="utf-8") as file:
+        for todos in url_user_todos:
+            data = f'"{id_employee}","{username}","{todos.get("completed")}",'
+            data2 = f'"{todos.get("title")}"\n'
+            file.write(data+data2)
