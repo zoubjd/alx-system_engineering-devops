91b57bf9250ca1332bffc368ad78c84fb45f674f
zoubjd <zouhairbajdouri19@gmail.com>
2024-08-18 22:03:47 +0100

    push

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -9,14 +9,12 @@ def number_of_subscribers(subreddit):
     from Reddit API
     """
     url = 'https://www.reddit.com/r/{}.json'.format(subreddit)
-    headers = {'User-Agent': 'My User Agent 1.0'}
+    headers = {'User-Agent': 'zoubjd'}
     r = requests.get(url, headers=headers,
                             allow_redirects=False)
 
     if r.status_code != 200:
         return 0
 
-    data = r.json()['data']
-    pages = data['children']
-    page_data = pages[0]['data']
-    return page_data['subreddit_subscribers']
+    data = r.json()
+    return data['data'].get('subscribers')
