193ea330f7c8d28e00c041f5d69edc1856c418fc
zoubjd <zouhairbajdouri19@gmail.com>
2024-08-18 01:13:36 +0100

    puh

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,28 +1,5 @@
-# 0-strace_is_your_friend.pp
-
-# Example: Ensure a directory has correct permissions
-file { '/path/to/directory':
-  ensure  => directory,
-  owner   => 'www-data',
-  group   => 'www-data',
-  mode    => '0755',
-  recurse => true,
+# fixes the bug
+exec { 'Fix typo in filename':
+  command  => 'sudo sed -i "s/.phpp/.php/" /var/www/html/wp-settings.php',
+  provider => shell,
 }
-
-# Example: Ensure a configuration file is correct
-file { '/etc/apache2/apache2.conf':
-  ensure  => file,
-  owner   => 'root',
-  group   => 'root',
-  mode    => '0644',
-  content => template('my_module/apache2.conf.erb'),
-  notify  => Service['apache2'],
-}
-
-# Example: Restart Apache service
-service { 'apache2':
-  ensure    => running,
-  enable    => true,
-  subscribe => File['/etc/apache2/apache2.conf'],
-}
-
