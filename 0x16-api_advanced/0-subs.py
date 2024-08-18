5a6bb07bca57de4ea9b477f5629185e2ec88f633
zoubjd <zouhairbajdouri19@gmail.com>
2024-08-18 02:35:04 +0100

    push

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+'''fetches from recdit api and prints the number of subscribers'''
+
+def number_of_subscribers(subreddit):
+    import requests
+
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    r = requests.get(url)
+    subs = r.json().get('data').get('subscribers')
+    print(subs)
