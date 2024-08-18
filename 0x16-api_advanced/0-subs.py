5ac9f96ba2cbe8caba6660f95bc2e8009f87cd80
zoubjd <zouhairbajdouri19@gmail.com>
2024-08-18 22:06:37 +0100

    push

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,20 +1,17 @@
 #!/usr/bin/python3
-'''fetches from recdit api and prints the number of subscribers'''
-
+"""fetches from api"""
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """Request number of subscribers of subreddit
-    from Reddit API
-    """
-    url = 'https://www.reddit.com/r/{}.json'.format(subreddit)
-    headers = {'User-Agent': 'zoubjd'}
-    r = requests.get(url, headers=headers,
-                            allow_redirects=False)
-
-    if r.status_code != 200:
+    """ returns number of subscribers """
+    url = 'https://www.reddit.com/r/{subreddit}/about.json'.format(subreddit)
+    headers = {
+        'User-Agent': 'custom-user-agent'
+        }
+    res = requests.get(url, headers=headers, allow_redirects=False)
+    if res.status_code == 200:
+        data = res.json()
+        return data['data'].get('subscribers')
+    else:
         return 0
-
-    data = r.json()
-    return data['data'].get('subscribers')
