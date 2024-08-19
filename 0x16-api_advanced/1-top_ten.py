f589362543a45a8749a6d92e1ea47b06761f26ed
zoubjd <zouhairbajdouri19@gmail.com>
2024-08-19 02:05:50 +0100

    push

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -6,15 +6,15 @@ import requests
 def top_ten(subreddit):
     """ Function that queries the Reddit API and prints
         the titles of the first 10 hot posts """
-    url = f'https://www.reddit.com/r/{subreddit}/hot.json?limit=10'
+    url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
     headers = {
         'User-Agent': 'custom-user-agent'
     }
     res = requests.get(url, headers=headers, allow_redirects=False)
     if res.status_code == 200:
-        data = res.json()
-        posts = data['data'].get('children', [])
-        for post in posts:
-            print(post['data']['title'])
+        data = res.json().get('data')
+        for post in data.get('children'):
+            print(post.get('data').get('title'))
     else:
-        print(None)
+        print("None")
+        return
