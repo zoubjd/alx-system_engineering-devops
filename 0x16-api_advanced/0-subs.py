e4b54e1e05fc31267dae92a7195d48bccab26580
zoubjd <zouhairbajdouri19@gmail.com>
2024-08-19 01:59:22 +0100

    push

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -11,7 +11,7 @@ def number_of_subscribers(subreddit):
         }
     res = requests.get(url, headers=headers, allow_redirects=False)
     if res.status_code == 200:
-        data = res.json()
-        return data['data'].get('subscribers')
+        data = res.json().get('data')
+        return data.get('subscribers')
     else:
         return 0
